ecc1e1ac90406633f44afb92227fd1d8b305f691
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2023-10-26 10:22:51 +0200

    Recipe API connect to endpoint

---
+++ backend/src/recipe-api.ts
@@ -0,0 +1,29 @@
+require('dotenv').config()
+
+const apiKey = process.env.API_KEY;
+
+export const searchRecipes = async (searchTerm: string, page: number) => {
+    if(!apiKey){
+        throw new Error("API Key not found!");
+    }
+
+    const url = new URL("https://api.spoonacular.com/recipes/complexSearch");
+
+    const queryParams = {
+        apiKey,
+        query: searchTerm,
+        number: "10",
+        offset: (page * 10).toString()
+    }
+
+    url.search = new URLSearchParams(queryParams).toString();
+
+    try {
+        const searchResponse = await fetch(url);
+        const resultsJSON = await searchResponse.json();
+
+        return resultsJSON;
+    } catch (error) {
+        console.log(error);
+    }
+}
