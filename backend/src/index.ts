e498599e7a4ff2df2f7c7f8bc557f403fe72299e
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2023-10-31 14:37:32 +0100

    recipe summary endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,6 +18,14 @@ app.get('/api/recipes/search', async (req, res) => {
     return res.json(results);
 })
 
+app.get("/api/recipes/:recipeId/summary", async (req, res) => {
+    const recipeId = req.params.recipeId;
+
+    const results = await RecipeAPI.getRecipeSummary(recipeId);
+
+    return res.json(results);
+})
+
 app.listen(8080, () => {
     console.log("Server running on port 8080");
 })
--- backend/src/recipe-api.ts
+++ backend/src/recipe-api.ts
@@ -28,7 +28,19 @@ export const searchRecipes = async (searchTerm: string, page: number) => {
     }
 };
 
-export const recipeSummary = async (recipeId: string) => {
+export const getRecipeSummary = async (recipeId: string) => {
+    if(!apiKey){
+        throw new Error("API Key not found")
+    }
     const url = new URL(`https://api.spoonacular.com/recipes/${recipeId}/summary`)
 
+    const params = {
+        apiKey
+    }
+     
+    url.search = new URLSearchParams(params).toString()
+    const response = await fetch(url);
+    const json = await response.json();
+
+    return json;
 }
